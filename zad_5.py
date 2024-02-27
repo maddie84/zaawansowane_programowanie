167b327d0e984830e97e6aa3f2983fcb149dc04e
Magda <jazwinskamagda@gmail.com>
2024-02-27 16:38:35 +0100

    Dodanie zadan 5 i 6

---
+++ zad_5.py
@@ -0,0 +1,18 @@
+# Stworzyć funkcję, która przyjmie 2 argumenty. Pierwszy typu list , a drugi
+# typu int . Funkcja ma sprawdzić (zwracając typ logiczny bool ), czy lista z
+# parametru pierwszego zawiera taką wartość jaką przekazano w parametrze
+# drugim.
+
+def czy_takie_same (lista=[], liczba=0):
+    for i in lista:
+        if i == liczba:
+            return True
+        else:
+            return False
+        
+liczby = [2]
+a = 2
+liczby2 = [3]
+b = 3
+print(czy_takie_same(liczby,a))
+print(czy_takie_same(liczby2,b))
