4090f065345aa4bed7ccc6699fb914709c711bbd
Magda <jazwinskamagda@gmail.com>
2024-02-27 16:54:34 +0100

    Zmiany

--- zad2a.py
+++
@@ -1,9 +0,0 @@
-# Utwórz funkcję, która otrzyma w parametrze listę 5 imion, a następnie wyświetli każde z nich.
-
-def wyswietl_imiona(imiona):
-    for imie in imiona:
-        print(imie)
-
-lista_imion = ["Anna", "Jan", "Maria", "Piotr", "Katarzyna"]
-
-wyswietl_imiona(lista_imion)
